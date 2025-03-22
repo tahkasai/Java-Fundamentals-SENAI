5bdaaf51fe0451f9bb36a7fb51d67e7129c79167
Tainá Kasai <136513139+tahkasai@users.noreply.github.com>
2025-03-22 10:40:34 -0300

    Update Main.java Aula04

--- Senai-Aula04/src/Main.java
+++ Senai-Aula04/src/Main.java
@@ -1,11 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         
-        System.out.printf("Hello and welcome!");
+        // Manipulação de caracteres 
 
-        for (int i = 1; i <= 5; i++) {
-            
-            System.out.println("i = " + i);
-        }
+        System.out.printIn("Digite três caracteres");
+
+        char char1 = scanner.next().charAt(0); // Lê o Primeiro caractere
+        char char2 = scanner.next().charAt(0); // Lê o Segundo caractere
+        char char3 = scanner.next().charAt(0); // Lê o Terceiro caractere
+
+        System.out.printIn("Os caracteres digitados foraam: "+char1+char2+char3);
+       
     }
 }
