d8da5793c7cd35e77beab40b40129b6c90c458b6
Tainá Kasai <tainakasai01@gmail.com>
2025-03-29 12:35:08 -0300

    Create class Escola

--- SistemaAcademico/src/models/Escola.java
+++ SistemaAcademico/src/models/Escola.java
@@ -1,4 +1,40 @@
 package models;
+import java.util.ArrayList;
 
 public class Escola {
+    private String nomeEscola;
+    private ArrayList<Aluno> alunos;
+    private ArrayList<Professor> professores;
+
+    public Escola(String nomeEscola) {
+        this.nomeEscola = nomeEscola;
+        this.alunos =  new ArrayList<>();
+        this.professores = new ArrayList<>();
+    }
+
+    public void adicionarAluno(Aluno aluno){
+        alunos.add(aluno);
+    }
+
+    public void adicionarProfessor(Professor professor){
+        professores.add(professor);
+    }
+
+    public void listaDeAlunos(){
+        System.out.println("Lista de Alunos da escola");
+        System.out.println("------------------------------------------");
+        for(Aluno aluno:alunos){
+            System.out.println(aluno);
+        }
+    }
+
+    public void listaDeProfessores(){
+        System.out.println("Lista de Professores da escola");
+        System.out.println("-----------------------------------------");
+        for(Professor professor:professores){
+            System.out.println(professor);
+        }
+    }
+
+    
 }
