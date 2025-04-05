be3a65760fe2c9599698ccea30b7d3d09d9650b5
Tainá Kasai <tainakasai01@gmail.com>
2025-04-05 15:49:48 -0300

    indentação da classe Academia

--- GestaoAcademica/src/Models/Academia.java
+++ GestaoAcademica/src/Models/Academia.java
@@ -11,16 +11,16 @@ public class Academia {
         this.pessoas = new ArrayList<>();
     }
 
-    public void adicionarPessoa(Pessoa pessoa){
+    public void adicionarPessoa(Pessoa pessoa) {
         pessoas.add(pessoa);
     }
 
-    public void listarPessoas(){
+    public void listarPessoas() {
         System.out.println("Lista de Pessoas da Academia");
         System.out.println("-----------------------------");
 
-        for (Pessoa pessoaDaLista:pessoas){
-            System.out.println("- "+pessoaDaLista.getDescricao());
+        for (Pessoa pessoaDaLista : pessoas) {
+            System.out.println("- " + pessoaDaLista.getDescricao());
         }
     }
 }
