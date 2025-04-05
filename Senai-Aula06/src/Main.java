abba7d3260fb2e7f913fe8927395e6353cf2a8f6
Tainá Kasai <tainakasai01@gmail.com>
2025-04-05 14:09:27 -0300

    Update

--- Senai-Aula06/src/Main.java
+++ Senai-Aula06/src/Main.java
@@ -1,13 +1,14 @@
-import Models.Carro;
+import Carro.Models.Carro;
 
 public class Main {
     public static void main(String[] args) {
         Carro carro = new Carro();
 
+
         carro.modelo = "Onix";
         carro.cor = "preto";
         carro.ano = 2024;
         carro.exibirInformacao();
-        
+
     }
 }
