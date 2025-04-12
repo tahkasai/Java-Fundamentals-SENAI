2d7fc561a42e0e16b70dab6cf2b0a65394028ecc
Tainá Kasai <tainakasai01@gmail.com>
2025-04-12 16:36:59 -0300

    update

--- SistemaAcademico-HTML-CSS-JAVA-e-BD/src/main/java/com/sistema/academico/AcademicoApplication.java
+++ SistemaAcademico-HTML-CSS-JAVA-e-BD/src/main/java/com/sistema/academico/AcademicoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AcademicoApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(AcademicoApplication.class, args);
 	}
 
