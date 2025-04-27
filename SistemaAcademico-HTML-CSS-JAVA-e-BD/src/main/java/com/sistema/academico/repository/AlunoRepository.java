3aacd0b156c184d14a7c45b58b6d6b8a8fa0f5ee
Tainá Kasai <136513139+tahkasai@users.noreply.github.com>
2025-04-27 12:59:49 -0300

    delete "sistema academico html css java"

--- SistemaAcademico-HTML-CSS-JAVA-e-BD/src/main/java/com/sistema/academico/repository/AlunoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sistema.academico.repository;
-
-import com.sistema.academico.model.Aluno;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlunoRepository extends JpaRepository<Aluno,Long> {
-
-}
--- SistemaAcademico-HTML-CSS-JAVA-e-BD/src/main/java/com/sistema/academico/repository/ProfessorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sistema.academico.repository;
-
-import com.sistema.academico.model.Professor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfessorRepository extends JpaRepository<Professor,Long> {
-
-
-}
